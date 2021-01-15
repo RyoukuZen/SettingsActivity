97b9d451f3738c3209761c9e9ba9435a7a5e6d8a
radaken <alexon249@gmail.com>
2021-01-15 20:31:09 +0300

    Добавил RecyclerView с заглушкой в виде массива

--- app/src/main/java/codergrad/bloknot/MainActivity.java
+++ app/src/main/java/codergrad/bloknot/MainActivity.java
@@ -1,37 +1,52 @@
 package codergrad.bloknot;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<State> states = new ArrayList<State>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        setInitialData();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+        // создаем адаптер
+        StateAdapter adapter = new StateAdapter(this, states);
+        // устанавливаем для списка адаптер
+        recyclerView.setAdapter(adapter);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                states.add(new State ("Бразилия", "Бразилиа", R.drawable.number_one));
             }
         });
     }
+    private void setInitialData(){
 
+        states.add(new State ("Бразилия", "Бразилиа", R.drawable.number_one));
+        states.add(new State ("Аргентина", "Буэнос-Айрес", R.drawable.number_two));
+        states.add(new State ("Колумбия", "Богота", R.drawable.number_three));
+        states.add(new State ("Уругвай", "Монтевидео", R.drawable.number_four));
+        states.add(new State ("Чили", "Сантьяго", R.drawable.number_five));
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
